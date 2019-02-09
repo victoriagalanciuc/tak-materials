3fbaa7388266db8e5d7791e737fa68dea2b5c6bb
Victoria Galanciuc <victoria.galanciuc@gmail.com>
2019-02-09 06:05:22 +0200

    Added materials for code breaker project

---
+++ Code Breaker/src/assets/main.js
@@ -0,0 +1,9 @@
+let answer = document.getElementById('answer');
+let attempt = document.getElementById('attempt');
+
+function guess() {
+    let input = document.getElementById('user-guess');
+    //add functionality to guess function here
+}
+
+//implement new functions here
