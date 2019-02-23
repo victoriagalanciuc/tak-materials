4a63015645069328dc440768dccb8dd04e0b224a
galanciuc <victoria.galanciuc@gmail.com>
2019-02-23 07:49:17 +0200

    Added tasks for js practice exercises

---
+++ JavaScript Exercises/session3/index_start.js
@@ -0,0 +1,45 @@
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all numbers
+// ex. addAll(2,5,6,7) === 20
+
+function addAll() {
+
+}
+
+// CHALLENGE 2: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. 
+// ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes() {
+
+}
+
+// CHALLENGE 3: SORT BY HEIGHT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight() {
+
+}
+
+// CHALLENGE 4: MISSING LETTERS
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters() {
+	
+}
+
+// CHALLENGE 5: EVEN & ODD SUMS
+// Take in an array and return an array of the sums of even and odd numbers
+// ex.
+// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
+
+function evenOddSums() {
+
+}
